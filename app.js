22a098b40a4c9c9ae48a84606c3071f711c859cf
Timits1 <will4f21@yahoo.com>
2019-07-30 04:50:15 -0700

    added firebase to page

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ document.getElementById('today').innerHTML += date.toDateString();
 // get the date as a string
 var n = date.toDateString();
 
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB8kZIn2lB_IzFDFgtKapluryTRMeXi3ik",
+    authDomain: "train-dff2e.firebaseapp.com",
+    databaseURL: "https://train-dff2e.firebaseio.com",
+    projectId: "train-dff2e",
+    storageBucket: "",
+    messagingSenderId: "976384710362",
+    appId: "1:976384710362:web:6cc071aa8f84f7cb"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
 var trainName = "";
 var destination = "";
 var startTime = "";
